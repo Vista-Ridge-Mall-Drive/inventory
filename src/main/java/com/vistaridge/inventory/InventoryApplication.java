54a42a0e205c6bfba3383e590639619abc710f90
magnustiberius <bgonzales20001@gmail.com>
2020-10-25 17:44:17 -0500

    user

--- src/main/java/com/vistaridge/inventory/InventoryApplication.java
+++ src/main/java/com/vistaridge/inventory/InventoryApplication.java
@@ -1,7 +1,12 @@
 package com.vistaridge.inventory;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import entities.User;
+import repository.UserRepository;
 
 @SpringBootApplication
 public class InventoryApplication {
@@ -10,4 +15,15 @@ public class InventoryApplication {
 		SpringApplication.run(InventoryApplication.class, args);
 	}
 
+//    @Bean
+//    public CommandLineRunner run(UserRepository userRepository) throws Exception {
+//        return (String[] args) -> {
+//            User user1 = new User("John", "john@domain.com");
+//            User user2 = new User("Julie", "julie@domain.com");
+//            userRepository.save(user1);
+//            userRepository.save(user2);
+//            userRepository.findAll().forEach(user -> System.out.println(user.getName()));
+//        };
+//    }	
+	
 }
---
+++ src/main/java/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import entities.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {}
